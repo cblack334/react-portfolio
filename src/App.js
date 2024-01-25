af453409da4044ee64e3e061da748d76f8c9f56a
cblack334 <156227079+cblack334@users.noreply.github.com>
2024-01-25 13:10:45 -0700

    headArt

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
 import githubimb from "./assetes/github.png"
 import linkedimb from "./assetes/linked_icon-icons.com_66731.png"
+import photoimg from "./assetes/photo.jpg"
 
 
 function App() {
   return (
     <div className="App">
-     <div className="Top">
 
-      <div className="github">
-        <img className="img2" src = {githubimb}/>
-      </div>
-      <div className="linkin">
-        <img className="img2"src = {linkedimb}/>
-      </div>
+      <div className="Top">
 
-     </div>
+        <div className="github">
+          <img className="img2" src={githubimb} />
+        </div>
+
+        <div className="linkin">
+          <img className="img1" src={linkedimb} />
+        </div>
+      </div>
+        <div className="headArt">
+          <div className="name">
+            <h1><b>Casey J.</b></h1> 
+            <h1><b>Black</b></h1>    
+          </div>
+          <div className="photo">
+            <img className="img3" src={photoimg} />
+
+          </div>
+          
+          
+
+
+        </div>
+
+     
     </div>
   );
 }
